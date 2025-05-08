9e0aadbd958a9d730afb8084ec9558500ce5ce6a
lmarcato <lmarcato@deloitte.it>
2025-05-08 15:58:30 +0200

    created route 'map_markers/all'

--- fastapi_service/app/map_marker/router.py
+++ fastapi_service/app/map_marker/router.py
@@ -11,17 +11,24 @@ from app.map_marker.models import MapMarker
 router = APIRouter()
 SessionDep = Annotated[Session, Depends(get_session)]
 
-@router.get('/map_markers/all')
+
+@router.get("/map_markers/all")
 def get_all_map_markers(session: SessionDep) -> list[MapMarkerOut]:
     try:
         map_markers = session.exec(select(MapMarker)).all()
 
-        if not map_markers: return JSONResponse(content={})
+        if not map_markers:
+            return JSONResponse(content={})
 
-        return MapMarkerOut.create_from_sql_model_map_marker(map_markers)
-    
+        return [
+            MapMarkerOut.create_from_sql_model_map_marker(map_marker)
+            for map_marker in map_markers
+        ]
+
     except HTTPException as http_exception:
-        return Response(content=http_exception.detail, status_code=http_exception.status_code)
-    
+        return Response(
+            content=http_exception.detail, status_code=http_exception.status_code
+        )
+
     except Exception as e:
-        logging.exception(f'Exception occurred in getting map markers: {e}')
+        logging.exception(f"Exception occurred in getting map markers: {e}")
