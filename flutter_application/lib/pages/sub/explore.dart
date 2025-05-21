7632735f3c4089b75341f854b80c91e9878089d8
lmarcato <lmarcato@deloitte.it>
2025-05-21 12:02:12 +0200

    added autocomplete search bar

--- flutter_application/lib/pages/sub/explore.dart
+++ flutter_application/lib/pages/sub/explore.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/models/mapmarkers.dart';
 import 'package:flutter_application/services/domain/mapmarker_service.dart';
 import 'package:flutter_application/widgets/exploremap.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Explore extends StatefulWidget {
   const Explore({super.key});
@@ -11,18 +13,103 @@ class Explore extends StatefulWidget {
 }
 
 class _ExploreState extends State<Explore> {
-  late Future<List<MapMarker>> _mapMarkersFuture ;
+  late Future<List<MapMarker>> _mapMarkersFuture;
+  late List<MapMarker> _mapMarkers;
+  MapMarker? _selectedMapMarker;
+  final MapController _mapController = MapController();
+  OverlayEntry? overlayEntry;
 
   @override
   void initState() {
     super.initState();
-    _mapMarkersFuture  =
-        _fetchMapMarkers();
+    _mapMarkersFuture = _fetchMapMarkers();
   }
 
   Future<List<MapMarker>> _fetchMapMarkers() async {
     final mapMarkerService = MapmarkerService();
-    return await mapMarkerService.getMapMarkerAll();
+    List<MapMarker> result = await mapMarkerService.getMapMarkerAll();
+
+    setState(() {
+      _mapMarkers = result;
+    });
+
+    return result;
+  }
+
+  void handlePlaceSelection(BuildContext context, MapMarker marker) {
+    setState(() {
+      _mapController.move(LatLng(marker.latitude, marker.longitude), 12);
+      _selectedMapMarker = marker;
+    });
+    _showOverlay(context);
+  }
+
+  void _showOverlay(BuildContext context) async {
+    removeHighlightOverlay();
+
+    final double width = MediaQuery.of(context).size.width;
+
+    overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          bottom: 0,
+          right: 0,
+          left: 0,
+          child: Container(
+            margin: EdgeInsets.only(left: 32, bottom: 32, right: 32),
+            child: SizedBox(
+              width: width,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      spacing: 4,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              _selectedMapMarker!.name,
+                              style: Theme.of(context).textTheme.bodyLarge
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            IconButton(
+                              onPressed: () => {removeHighlightOverlay()},
+                              icon: Icon(Icons.close),
+                            ),
+                          ],
+                        ),
+                        Text(_selectedMapMarker!.type),
+                        Text(_selectedMapMarker!.description ?? ''),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    // inserting overlay entry
+    Overlay.of(context, debugRequiredFor: widget).insert(overlayEntry!);
+  }
+
+  void removeHighlightOverlay() {
+    overlayEntry?.remove();
+    overlayEntry?.dispose();
+    overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    removeHighlightOverlay();
+    super.dispose();
   }
 
   @override
@@ -53,24 +140,36 @@ class _ExploreState extends State<Explore> {
 
           Padding(
             padding: const EdgeInsets.only(left: 24, right: 24, top: 16),
-            child: SearchBar(
-              leading: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Icon(
-                  Icons.search,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-              onTap: () {},
-              onChanged: (value) {},
-              onSubmitted: (value) {},
+            child: Autocomplete<String>(
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text == '') {
+                  return const Iterable<String>.empty();
+                }
+                return _mapMarkers
+                    .where((marker) {
+                      return marker.name.toLowerCase().contains(
+                        textEditingValue.text.toLowerCase(),
+                      );
+                    })
+                    .map((marker) => marker.name);
+              },
+              onSelected: (String selection) {
+                var marker = _mapMarkers.singleWhere(
+                  (marker) =>
+                      marker.name.toLowerCase() == selection.toLowerCase(),
+                );
+                handlePlaceSelection(context, marker);
+              },
             ),
           ),
-
           Expanded(
             child: Padding(
               padding: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
-              child: ExploreMapWidget(mapMarkersFuture: _mapMarkersFuture ),
+              child: ExploreMapWidget(
+                mapMarkersFuture: _mapMarkersFuture,
+                mapController: _mapController,
+                onMapMarkerTap: handlePlaceSelection,
+              ),
             ),
           ),
         ],
--- flutter_application/lib/widgets/exploremap.dart
+++ flutter_application/lib/widgets/exploremap.dart
@@ -6,107 +6,29 @@ import 'package:logger/logger.dart';
 import 'package:flutter_application/constants.dart' as constants;
 import 'package:url_launcher/url_launcher.dart';
 
-class ExploreMapWidget extends StatefulWidget {
-  ExploreMapWidget({super.key, required this.mapMarkersFuture});
+class ExploreMapWidget extends StatelessWidget {
+  ExploreMapWidget({
+    super.key,
+    required this.mapMarkersFuture,
+    required this.mapController,
+    required this.onMapMarkerTap,
+  });
 
-  final Future<List<MapMarker>> mapMarkersFuture; // Accetta il Future
+  final Future<List<MapMarker>> mapMarkersFuture;
+  final MapController mapController;
+  final Function(BuildContext context, MapMarker marker) onMapMarkerTap;
   final Logger logger = Logger();
-
-  @override
-  State<ExploreMapWidget> createState() => _ExploreMapWidgetState();
-}
-
-class _ExploreMapWidgetState extends State<ExploreMapWidget>
-    with TickerProviderStateMixin {
-  LatLng mapCenter = LatLng(46.433334, 11.850000); // Example coordinates
-
-  AnimationController? animationController;
-  Animation<double>? animation;
-
-  @override
-  void initState() {
-    super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    animation = CurveTween(
-      curve: Curves.fastOutSlowIn,
-    ).animate(animationController!);
-  }
-
-  void _showOverlay(BuildContext context, {required MapMarker marker}) async {
-    final double width = MediaQuery.of(context).size.width;
-    OverlayState? overlayState = Overlay.of(context);
-    OverlayEntry overlayEntry;
-
-    overlayEntry = OverlayEntry(
-      builder: (context) {
-        return Positioned(
-          bottom: 0,
-          right: 0,
-          left: 0,
-          child: Container(
-            margin: EdgeInsets.only(left: 32, bottom: 32, right: 32),
-            child: SizedBox(
-              width: width,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      spacing: 4,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              marker.name,
-                              style: Theme.of(context).textTheme.bodyLarge
-                                  ?.copyWith(fontWeight: FontWeight.bold),
-                            ),
-                            IconButton(
-                              onPressed: () => {animationController!.reverse()},
-                              icon: Icon(Icons.close),
-                            ),
-                          ],
-                        ),
-                        Text(marker.type),
-                        Text(marker.description ?? ''),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-    animationController!.addListener(() {
-      overlayState.setState(() {});
-
-      if (animation!.value == 1 && !animation!.isForwardOrCompleted) {
-        overlayEntry.remove();
-      }
-    });
-    // inserting overlay entry
-    overlayState.insert(overlayEntry);
-    animationController!.forward();
-  }
+  final LatLng mapCenter = LatLng(46.433334, 11.850000); // Example coordinates
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<MapMarker>>(
-      future: widget.mapMarkersFuture,
+      future: mapMarkersFuture,
       builder: (BuildContext context, AsyncSnapshot<List<MapMarker>> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          widget.logger.e('Error loading map markers: ${snapshot.error}');
+          logger.e('Error loading map markers: ${snapshot.error}');
           return const Center(child: Text('Error loading data'));
         } else if (snapshot.hasData) {
           final List<MapMarker> loadedMapMarkers = snapshot.data!;
@@ -115,6 +37,7 @@ class _ExploreMapWidgetState extends State<ExploreMapWidget>
             child: Stack(
               children: [
                 FlutterMap(
+                  mapController: mapController,
                   options: MapOptions(
                     initialCenter: mapCenter,
                     initialZoom: 12,
@@ -133,8 +56,9 @@ class _ExploreMapWidgetState extends State<ExploreMapWidget>
                               width: 40,
                               height: 40,
                               child: GestureDetector(
-                                onTap:
-                                    () => _showOverlay(context, marker: marker),
+                                onTap: () {
+                                  onMapMarkerTap(context, marker);
+                                },
                                 child: Image(
                                   image: AssetImage(
                                     constants.markerType2IconData[marker.type]!,
